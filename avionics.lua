0533b26d6d70cd3e575f7be2f7f8d579e9294453
John Bray <johnbray@Johns-Mac-mini.local>
2020-02-21 00:02:44 +0100

    panel

--- avionics.lua
+++ avionics.lua
@@ -7,8 +7,8 @@ defineProperty("iPad1_mode",createGlobalPropertyf("jb/sasl/iPads/1/mode"))  -- 0
 set(iPad1_mode, 0)
 
 defineProperty("iPadVisibility",createGlobalPropertyi("jb/sasl/iPads/visible"))  -- 0=off, 1= ON (INT)
---set(iPadVisibility,  0) 
-set(iPadVisibility,  1)
+set(iPadVisibility,  0) 
+--set(iPadVisibility,  1)
 
 defineProperty("HudHidden",     createGlobalPropertyi("jb/sasl/Hud/hidden"))      -- 0=visible, 1= hidden (killed) (INT)
 defineProperty("airframeHidden",createGlobalPropertyi("jb/sasl/Airframe/hidden")) -- 0=visible, 1= hidden (killed) (INT)
